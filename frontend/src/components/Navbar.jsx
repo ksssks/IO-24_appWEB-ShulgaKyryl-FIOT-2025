0a8074225d4e895cae0edfc577650d8b19cc7f6d
unknown <kyrylshulga.in@gmail.com>
2025-02-18 16:31:39 +0200

    adding gh pages settings 9

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,19 +13,19 @@ const Navbar = () => {
         <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
             <img onClick={()=>navigate('/')} className='w-20 h-20 cursor-pointer' src={assets.logo} alt=''/>
             <ul className='hidden md:flex items-start gap-5 font-medium'>
-                <NavLink to={'/'}>
+                <NavLink to={'/IO-24_appWEB-ShulgaKyryl-FIOT-2025/'}>
                     <li className='py-1'>Домашня</li>
                     <hr className='border-none outine-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
                 </NavLink>
-                <NavLink to={'/doctors'}>
+                <NavLink to={'/IO-24_appWEB-ShulgaKyryl-FIOT-2025/doctors'}>
                     <li className='py-1'>Усі лікарі</li>
                     <hr className='border-none outine-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
                 </NavLink>
-                <NavLink to={'/about'}>
+                <NavLink to={'/IO-24_appWEB-ShulgaKyryl-FIOT-2025/about'}>
                     <li className='py-1'>Про нас</li>
                     <hr className='border-none outine-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
                 </NavLink>
-                <NavLink to={'/contact'}>
+                <NavLink to={'/IO-24_appWEB-ShulgaKyryl-FIOT-2025/contact'}>
                     <li className='py-1'>Контакти</li>
                     <hr className='border-none outine-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
                 </NavLink>
@@ -38,12 +38,12 @@ const Navbar = () => {
                         <img src={assets.dropdown} className='w-6' alt='Dropdown icon'/>
                         <div className='absolute top-8 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                             <div className='min-w-48 bg-stone-200 rounded flex flex-col gap-4 p-4'>
-                                <p onClick={()=>navigate('/my-profile')} className='hover:text-black'>Мій профіль</p>
-                                <p onClick={()=>navigate('/my-appointments')} className='hover:text-black'>Мої сеанси</p>
+                                <p onClick={()=>navigate('/IO-24_appWEB-ShulgaKyryl-FIOT-2025/my-profile')} className='hover:text-black'>Мій профіль</p>
+                                <p onClick={()=>navigate('/IO-24_appWEB-ShulgaKyryl-FIOT-2025/my-appointments')} className='hover:text-black'>Мої сеанси</p>
                                 <p onClick={()=>setToken(false)} className='hover:text-black'>Вийти</p>
                             </div>
                         </div>
-                        </div> : <button onClick={() => navigate('/login')}
+                        </div> : <button onClick={() => navigate('/IO-24_appWEB-ShulgaKyryl-FIOT-2025/login')}
                                                 className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Створити
                             акаунт
                         </button>
